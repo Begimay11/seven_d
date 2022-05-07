4d2d935a532d52dea4b68c9dd272c09f88969496
Begimay11 <begimaysagynbaeva245@gmail.com>
2022-05-07 14:52:24 +0600

    added-ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Button } from './components/Button/Button';
+import { Counter } from './components/Count/Counter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+      <Counter counter = '0'/>
+     <Button title = '+'/>
+     <Button title = '-'/>
     </div>
   );
 }
